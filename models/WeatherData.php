e106f8a9a864c47a93a736671b1ef396d9a15c3d
guar47 <guar47@ya.ru>
2017-04-02 14:23:15 +0300

    add change path & add data picker

---
+++ models/WeatherData.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models;
+use yii\base\Model;
+
+class WeatherData extends Model
+{
+    public $from_date;
+    public $to_date;
+}
---
+++ views/site/index.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\jui\DatePicker;
+
+$this->title = 'Weather statistics';
+?>
+<div class="site-index">
+    <?php
+    echo DatePicker::widget([
+//        'model' => $model,
+        'attribute' => 'from_date',
+        'value' => '2016-01-01',
+        'dateFormat' => 'yyyy-MM-dd',
+    ]);
+    echo DatePicker::widget([
+//        'model' => $model,
+        'attribute' => 'to_date',
+        'value'  => '2016-02-01',
+        'dateFormat' => 'yyyy-MM-dd',
+    ]);
+    ?>
+</div>
+
+
