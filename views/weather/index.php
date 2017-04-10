961157a78051f202e8311c97ed7ae01fba79faf9
guar47 <guar47@ya.ru>
2017-04-10 12:21:39 +0300

    add week numbers to table

--- views/weather/index.php
+++ views/weather/index.php
@@ -12,7 +12,7 @@ $this->title = 'Weather statistics index';
     <table class="table">
         <thead>
         <tr>
-            <!--            <th></th>-->
+                        <th></th>
             <th></th>
             <th>Январь</th>
             <th></th>
@@ -22,7 +22,7 @@ $this->title = 'Weather statistics index';
             <th></th>
         </tr>
         <tr>
-            <!--            <th>Неделя</th>-->
+                        <th>Неделя</th>
             <th>ПН</th>
             <th>ВТ</th>
             <th>СР</th>
@@ -40,37 +40,42 @@ $this->title = 'Weather statistics index';
             $table_row = '<td>' . date('d M', strtotime($weather_day['date'])) . '(' . $weather_day['day_temp'] .
                 ' / ' . $weather_day['night_temp'] . ')' . '</td>';
             $table_empty = '<td>' . '</td>';
+            $table_week_number = '<td>' . $weather_day['week_number'] . '</td>';
+
             if (date('d', strtotime($weather_day['date'])) === '01') {
                 switch ($weather_day['week_day']) {
                     case '0':
                         echo $table_row . '</tr>' . '<tr>';
                         break;
                     case '1':
-                        echo $table_row;
-                        break;
-                    case '2':
                         echo $table_empty . $table_row;
                         break;
-                    case '3':
+                    case '2':
                         echo $table_empty . $table_empty . $table_row;
                         break;
+                    case '3':
+                        echo $table_empty . $table_empty . $table_empty . $table_row;
+                        break;
                     case '4':
-                        echo $table_empty . $table_empty .
+                        echo $table_empty . $table_empty . $table_empty .
                             $table_empty . $table_row;
                         break;
                     case '5':
-                        echo $table_empty . $table_empty .
+                        echo $table_empty . $table_empty . $table_empty .
                             $table_empty . $table_empty . $table_row;
                         break;
                     case '6':
-                        echo $table_empty . $table_empty .
+                        echo $table_empty . $table_empty . $table_empty .
                             $table_empty . $table_empty . $table_empty . $table_row;
                         break;
                 }
             } else {
                 if ($weather_day['week_day'] === '0') {
                     echo $table_row . '</tr>' . '<tr>';
-                } else {
+                } elseif ($weather_day['week_day'] === '1') {
+                    echo $table_week_number . $table_row;
+                }
+                else {
                     echo $table_row;
                 }
             }
